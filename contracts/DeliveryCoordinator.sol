8d45032f7a2eb7a0bf5e6ad638fd45ebf96187d9
James Taylor <admin@tailoredit.co.za>
2022-04-07 17:14:38 +0200

    Implemented minting of receipt tokens

--- contracts/DeliveryCoordinator.sol
+++ contracts/DeliveryCoordinator.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol';
 
 import "./DeliveryNode.sol";
 import "./PackageToken.sol";
 import "./ReceiptToken.sol";
 
-contract DeliveryCoordinator is Ownable {
+contract DeliveryCoordinator is ERC721Holder, Ownable {
 
   DeliveryNode[] public deliveryNodes;
   address public packageTokenAddress;
@@ -25,6 +26,29 @@ contract DeliveryCoordinator is Ownable {
   function createPackage(string memory _packageContents, string memory _packageWeight) public {
     PackageToken deployedTokenContract = PackageToken(packageTokenAddress);
     deployedTokenContract.createPackage(msg.sender, _packageContents, _packageWeight);
+  }
+
+  function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data) public override returns (bytes4) {
+      require(msg.sender == packageTokenAddress || msg.sender == receiptTokenAddress, "ERC721 received is not from known token address");
+
+      if (msg.sender == packageTokenAddress) {
+        packageTokenReceived(from, tokenId);
+      }
+
+      if (msg.sender == receiptTokenAddress) {
+        receiptTokenReceived(from, tokenId);
+      }
+
+      return super.onERC721Received(operator, from, tokenId, data);
+  }
+
+  function packageTokenReceived(address from, uint256 packageTokenId) internal {
+    ReceiptToken deployedTokenContract = ReceiptToken(receiptTokenAddress);
+    deployedTokenContract.createReceipt(from, packageTokenId);
+  }
+
+  function receiptTokenReceived(address, uint256) pure internal {
+    require(false, "Receiving receipt tokens is not yet implemented");
   }
 
 }
--- contracts/ReceiptToken.sol
+++ contracts/ReceiptToken.sol
@@ -2,9 +2,27 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
 
 contract ReceiptToken is ERC721Burnable {
-  
+    
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
+    mapping(uint256 => uint256) public receiptData;
+
     constructor() ERC721("ReceiptToken", "RPT") {}
+
+    function createReceipt(address packageSender, uint256 packageTokenId) public returns (uint256) {
+        // TODO - assert that the sender owns the package in question or restrict access to this method to the delivery coordinator only
+        _tokenIds.increment();
+        
+        uint256 newReceiptId = _tokenIds.current();
+        _safeMint(packageSender, newReceiptId);
+        receiptData[newReceiptId] = packageTokenId;
+
+        return newReceiptId;
+    }
 
 }
