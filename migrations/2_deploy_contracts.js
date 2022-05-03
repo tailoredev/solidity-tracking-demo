f4e0dcf4061f92ef18543c6fb22a908b1af006e4
James Taylor <admin@tailoredit.co.za>
2022-05-03 16:57:55 +0200

    Displayed deployed contract addresses in UI

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var DeliveryCoordinator = artifacts.require("./DeliveryCoordinator.sol");
 
 module.exports = async function(deployer) {
-  deployer.deploy(SimpleStorage);
   await deployer.deploy(DeliveryCoordinator); 
 };
